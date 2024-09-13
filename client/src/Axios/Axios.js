368d47487fba788ef1dcc6795006ccb179e4ee91
fares_mohamed <113051677+fares-code@users.noreply.github.com>
2024-09-13 21:43:29 +0300

    Update Axios.js

--- client/src/Axios/Axios.js
+++ client/src/Axios/Axios.js
@@ -3,7 +3,7 @@ import Cookie from 'cookie-universal'
 const cookies = Cookie();
 const token =cookies.get("Iot_Project");
 export const Axios = axios.create({
-    baseURL:"https://iot-web-site-server.vercel.app",
+    baseURL:"https://iot-web-site-server.vercel.app/api/v1/auth/",
 headers:{
     Authorization:token
 }
